0a0cb32bf8ed73c35549e8f83e88c10b4e9384dc
Vladislava <heshtegvlada@gmail.com>
2023-07-28 17:44:03 +0300

    тест

--- script7.js
+++ script7.js
@@ -301,9 +301,9 @@
 //   return total / args.length;
 // }
 
-// console.log(calсulateAverage(1, 2, 3, 4)); // 2.5
-// console.log(calсulateAverage(14, 8, 2)); // 8
-// console.log(calсulateAverage(27, 43, 2, 8, 36)); // 23.2
+console.log(calсulateAverage(1, 2, 3, 4)); // 2.5
+console.log(calсulateAverage(14, 8, 2)); // 8
+console.log(calсulateAverage(27, 43, 2, 8, 36)); // 23.2
 
 const listWithId = document.querySelector('#menu');
 listWithId.style.textTransform = 'uppercase';
